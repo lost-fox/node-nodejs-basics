c281331f367901a09e8ea0ae5998c59bbf5d3d2c
lost-fox <Oljaburak@mail.ru>
2022-11-29 18:21:22 +0300

    feat: add solution fs

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
 const copy = async () => {
-    // Write your code here 
+    fs.stat('./files_copy', (err) => {
+        if (err == null) {
+           throw new Error ('FS operation failed');
+        }
+    });
+    fs.stat('./files', (err) => {
+        if (!err) {
+           fs.mkdir('files_copy', (err) => {
+                if(err) throw err;
+            });
+            fs.readdir('./files', (err, files) => {
+                if(err) throw err;
+                for(let i=0; i<files.length; i++){
+                    fs.copyFile(`./files/${files[i]}`,`./files_copy/${files[i]}`, (err) => {
+                        if(err) throw err;
+                    } )
+                }
+            })
+        } else if (err.code === 'ENOENT') {
+            throw new Error ('FS operation failed');  
+        }
+    })
+
 };
 
 copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
 const rename = async () => {
-    // Write your code here 
+   const wrongPath = './files/wrongFilename.txt';
+   const rightPath = './files/properFilename.md';
+   
+   fs.stat(rightPath, (err) => {
+        if (!err) throw new Error('FS operation failed');
+   });
+
+   fs.stat(wrongPath, (err) => {
+    if(err.code === 'ENOENT') {
+        throw new Error('FS operation failed');
+    } else if (!err) {
+        fs.rename(wrongPath, rightPath, (err) => {
+            if(err) throw err;
+        })
+    }
+   })
 };
 
 await rename();
