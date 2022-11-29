3b39e17a55e1bc815643c1545ebf800c57744aa3
lost-fox <Oljaburak@mail.ru>
2022-11-29 19:26:00 +0300

    feat: add solution cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argv = process.argv.slice(2);
+    const answ = [];
+    for (let i=0; i<argv.length;i+=2){
+        answ.push(`${argv[i]} is ${argv[i+1]}`)
+    }
+    console.log(answ.join(', '));
 };
 
 parseArgs();
