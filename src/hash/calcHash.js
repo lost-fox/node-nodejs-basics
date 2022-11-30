572a74e47f90a795983012d4202c25d7eb868bdf
lost-fox <Oljaburak@mail.ru>
2022-11-30 15:50:25 +0300

    feat: add solution hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,10 @@
 const calculateHash = async () => {
-    // Write your code here 
+    const {createHash} = await import('node:crypto');    
+    const hash = createHash('sha256');
+    const path = './files/fileToCalculateHashFor.txt';
+
+    hash.update(path);
+    console.log(hash.digest('hex'));
 };
 
 await calculateHash();
