16725d75ce90b3dd1280ff48f31feff37b8f430c
lost-fox <Oljaburak@mail.ru>
2022-12-05 15:18:22 +0300

    feat: add solution wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
 const performCalculations = async () => {
-    // Write your code here
+    const {cpus} = await import('node:os');
+    const {Worker} = await import('node:worker_threads');
+    const workerFile = './src/wt/worker.js';
+    const promiseArray = [];
+
+    const runService = (i) => {
+        return new Promise((resolve, reject) =>{
+            const worker = new Worker(workerFile, {
+                workerData: 10 + i
+            });
+            worker.on('message', resolve);
+            worker.on('error', reject);
+        })
+    }
+
+    for(let i=0; i<=cpus().length; i++) {
+        promiseArray.push(runService(i));
+    }
+
+    const promiseData = await Promise.allSettled(promiseArray);
+
+    const result = promiseData.map((item) => {
+        return {
+            status: item.status === 'fulfilled' ? 'resolved' : 'error',
+            data: item.status === 'fulfilled' ? item.value : 'null',
+        };
+    })
+
+    console.log(result);
 };
 
 await performCalculations();
