679c70ee25cbf582fa46e7288619866fafacaf84
lost-fox <Oljaburak@mail.ru>
2022-12-05 12:07:40 +0300

    feat: add solution zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
 const compress = async () => {
-    // Write your code here 
+   const {createGzip} = await import('node:zlib');
+   const { pipeline } = await import('node:stream');
+   const fs = await import('node:fs');
+   const zlibPath = './src/zip/files/fileToCompress.txt';
+   const gzPath = './src/zip/archive.gz';
+   
+   const fileStream = fs.createReadStream(zlibPath, {encoding: 'utf-8'});
+   const writeStream = fs.createWriteStream(gzPath);
+
+   const gzip = createGzip();
+
+   pipeline(fileStream, gzip, writeStream, (err) => {
+      if (err) {
+         console.error('An error occurred:', err);
+         process.exitCode = 1;
+      }
+       fs.unlink(zlibPath, (err) => {
+       if(err) throw err;
+   })
+   })
 };
 
 await compress();
